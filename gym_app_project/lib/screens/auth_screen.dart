1853c61abde565cf0531eae8be3ee1ae9a05152b
PrzemekZiemiakowski <przemo1814@gmail.com>
2022-02-17 02:16:22 +0100

    poprawki auth

--- gym_app_project/lib/screens/auth_screen.dart
+++ gym_app_project/lib/screens/auth_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gym_app_project/widgets/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -9,12 +13,33 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
   void _sumbitAuthForm(
     String email,
     String password,
     String username,
     bool isLogin,
-  ) {}
+  ) async {
+    // ignore: unused_local_variable
+    UserCredential authResult;
+    try {
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+      }
+    } on PlatformException catch (err) {
+      String? message = 'An error occurred, pleasae check your cred';
+      if (err.message != null) {
+        message = err.message;
+      }
+    } catch (err) {
+      print(err);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
